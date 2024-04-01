085303f4a3535ba86849f19670f5b20cca5c2e84
Zakariae <Zakariaeabassioff@gmail.com>
2024-04-01 02:08:58 +0200

    Fetch des paths depuis la BDD et selection de la couleur depuis un couleur picker.

--- DoodleConnect/src/pages/private/Home/components/SketchBoard.js
+++ DoodleConnect/src/pages/private/Home/components/SketchBoard.js
@@ -1,29 +1,63 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, StyleSheet, View, Modal } from "react-native";
 import { Canvas } from "@benjeau/react-native-draw";
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, onSnapshot, getDocs } from "firebase/firestore";
 import {
   FIREBASE_AUTH,
   FIREBASE_DB,
 } from "../../../../../firebase/firebaseConfig";
+import ColorPicker, {
+  Panel1,
+  Swatches,
+  Preview,
+  OpacitySlider,
+  HueSlider,
+} from "reanimated-color-picker";
 
 function SketchBoard() {
   const canvasRef = useRef(null);
   const [drawingPaths, setDrawingPaths] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [currentColor, setCurrentColor] = useState("#000");
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(
-      collection(FIREBASE_DB, "drawings"),
-      (snapshot) => {
-        const newDrawings = snapshot.docs.map((doc) => ({
+    const fetchDrawings = async () => {
+      try {
+        const querySnapshot = await getDocs(
+          collection(FIREBASE_DB, "drawings")
+        );
+        const drawingData = querySnapshot.docs.map((doc) => ({
           id: doc.id,
           ...doc.data(),
         }));
-        setDrawingPaths(newDrawings);
+        setDrawingPaths(drawingData);
+      } catch (error) {
+        console.error("Error fetching drawings:", error);
       }
-    );
-    return () => unsubscribe();
+    };
+
+    fetchDrawings();
   }, []);
+
+  console.log(drawingPaths);
+
+  const addPathsToCanvas = () => {
+    drawingPaths.forEach((drawing) => {
+      drawing.paths.forEach((path) => {
+        canvasRef.current?.addPath({
+          color: path.color,
+          thickness: path.thickness,
+          opacity: path.opacity,
+          data: path.data ? path.data.map((data) => JSON.parse(data)) : [],
+          path: path.path ? path.path.map(JSON.parse) : [],
+        });
+      });
+    });
+  };
+
+  useEffect(() => {
+    addPathsToCanvas();
+  }, [drawingPaths]);
 
   const handleSaveDrawing = async () => {
     const paths = canvasRef.current?.getPaths();
@@ -69,20 +103,41 @@ function SketchBoard() {
     canvasRef.current?.clear();
   };
 
+  const onSelectColor = ({ hex }) => {
+    // do something with the selected color.
+    setCurrentColor(hex);
+  };
+
   /*   console.log(drawingPaths[0]?.paths[0]);
    */ return (
     <View style={styles.container}>
       <Canvas
         ref={canvasRef}
         height={400}
-        color="red"
+        color={currentColor}
         thickness={20}
         opacity={0.6}
         style={{ backgroundColor: "lightgray" }}
       />
+      <Button title="Color Picker" onPress={() => setShowModal(true)} />
       <Button title="Undo" onPress={handleUndo} />
       <Button title="Save" onPress={handleSaveDrawing} />
       <Button title="Clear" onPress={handleClear} />
+      <Modal visible={showModal} animationType="slide">
+        <ColorPicker
+          style={{ width: "100%" }}
+          value={currentColor}
+          onComplete={onSelectColor}
+        >
+          <Preview />
+          <Panel1 />
+          <HueSlider />
+          <OpacitySlider />
+          <Swatches />
+        </ColorPicker>
+
+        <Button title="Ok" onPress={() => setShowModal(false)} />
+      </Modal>
     </View>
   );
 }
